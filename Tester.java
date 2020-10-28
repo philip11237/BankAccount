ca4db035b4d0e740b20fe7a0b6f95d77db69848d
philip von mueffling <philip.vm3@gmail.com>
2020-10-27 22:22:28 -0400

    test password

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester{
+  public static void main(String []args){
+    BankAccount test=new BankAccount("testpword", 215756545);
+    System.out.println (test.getPassword());
+    System.out.println (test.getAccountID());
+    System.out.println (test.getBalance());
+    test.setPassword("testpword2");
+    System.out.println (test.getPassword());
+
+  }
